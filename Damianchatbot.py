f68d22d80c80fc5b0c2ce6d5ef859466411473da
Damian0099 <damianvinodh@gmail.com>
2024-12-08 12:22:26 +0000

    Add files via upload

---
+++ Damianchatbot.py
@@ -0,0 +1,8 @@
+print("welcome to Damian chatbot")
+name=input('may I please know your name')
+print("hello",name,"it is nice meeting you")
+feeling=input("how are you feeling today? ")
+if feeling=="good":
+    print("thats great to hear")
+else:
+    print("hope you feel better")
